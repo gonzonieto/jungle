b4559520b5b57c5a6e3517fe666db265c7430ec2
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-05-07 22:37:23 -0400

    update name of Admin base controller

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,4 @@
-class Admin::AdminController < ApplicationController
+class Admin::BaseController < ApplicationController
 
   http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PW']
   
