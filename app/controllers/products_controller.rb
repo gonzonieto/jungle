ea149d385d474a92e94cf77410e8c9b09df66fa3
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-05-07 20:09:33 -0400

    add subtitle randomizer

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,13 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    @subtitle = [
+      'Where you can find any plants!',
+      "We've got fun and games!",
+      'We take it day by day',
+      'It gets worse here every day',
+      'Feel my, my, my serpentine'
+    ].sample
   end
 
   def show
